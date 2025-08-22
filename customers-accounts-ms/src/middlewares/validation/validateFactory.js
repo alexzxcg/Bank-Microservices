c8260e700c442d029b6c3dbf465d9d4e0b58da4e
Alex Rodrigues <alex-xp123@hotmail.com>
2025-08-22 16:36:35 -0400

    feat(middlewares): add validateFactory

---
+++ customers-accounts-ms/src/middlewares/validation/validateFactory.js
@@ -0,0 +1,17 @@
+const yup = require('yup');
+const { AppError } = require('../error/errorHandler');
+
+module.exports = (schema) => async (req, _res, next) => {
+  try {
+    req.body = await schema.validate(req.body, {
+      abortEarly: false,
+      stripUnknown: true,
+    });
+    return next();
+  } catch (err) {
+    if (err instanceof yup.ValidationError) {
+      return next(new AppError('Validation error', 400, err.errors));
+    }
+    return next(new AppError('Internal error while validating payload', 500));
+  }
+};
