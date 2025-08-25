c8171f721d09e894ea649c00392aad5251da38eb
Alex Rodrigues <alex-xp123@hotmail.com>
2025-08-25 11:38:57 -0400

    refactor(routes): use loginSchema

--- customers-accounts-ms/src/routes/authRoute.js
+++ customers-accounts-ms/src/routes/authRoute.js
@@ -1,15 +1,10 @@
 const { Router } = require('express');
 const AuthController = require('../controllers/AuthController');
 const validate = require('../middlewares/validation/validateFactory');
-const yup = require('yup');
+const { loginSchema } = require('../middlewares/validation/schemas/loginSchema');
 
 const router = Router();
 const controller = new AuthController();
-
-const loginSchema = yup.object({
-  email: yup.string().email().required(),
-  password: yup.string().required(),
-});
 
 router.post('/auth/login', validate(loginSchema), controller.login);
 
