4c0b8052acb3cd4b1be9b149af1ee118b7cc872b
Alex Rodrigues <alex-xp123@hotmail.com>
2025-09-04 12:10:12 -0400

    test(env): adjust .env.test loading with absolute path

--- customers-accounts-ms/tests/e2e/setup/env.js
+++ customers-accounts-ms/tests/e2e/setup/env.js
@@ -1,3 +1,6 @@
 process.env.NODE_ENV = 'test';
 
-require('dotenv').config({ path: '.env.test' });
+const path = require('path');
+require('dotenv').config({
+  path: path.resolve(__dirname, '../../../.env.test')
+});
