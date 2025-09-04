7f65509f9046ef1a4874576120bc582b4cf9fb5b
Alex Rodrigues <alex-xp123@hotmail.com>
2025-09-04 10:36:01 -0400

    refactor(validation): normalize email in login schema

--- customers-accounts-ms/src/middlewares/validation/schemas/loginSchema.js
+++ customers-accounts-ms/src/middlewares/validation/schemas/loginSchema.js
@@ -1,7 +1,12 @@
 const yup = require('yup');
+const { toTrim, toLower } = require('./_sharedTransforms');
 
 const loginSchema = yup.object({
-  email: yup.string().email('Invalid email').required('Email is required'),
+  email: yup
+    .string()
+    .transform((v) => (v ? toLower(toTrim(v)) : v))
+    .email('Invalid email')
+    .required('Email is required'),
   password: yup.string().required('Password is required'),
 });
 
