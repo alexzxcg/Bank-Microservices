74b68a5e7f6f3d563ca530c851d2dfd1120a8f07
Alex Rodrigues <alex-xp123@hotmail.com>
2025-09-04 10:26:33 -0400

    refactor(account-controller): unify method names and add ctx support

--- customers-accounts-ms/src/controllers/AccountController.js
+++ customers-accounts-ms/src/controllers/AccountController.js
@@ -1,62 +1,71 @@
 const Controller = require('./Controller.js');
-const AccountServices = require('../services/AccountServices.js');
+const { asyncHandler } = require('../middlewares/error/errorHandler.js');
 
+const AccountServices = require('../services/AccountServices.js');
 const AccountInputDTO = require('../dtos/account-dto/AccountInputDTO.js');
 const AccountUpdateDTO = require('../dtos/account-dto/AccountUpdateDTO.js');
 const AccountBalanceDTO = require('../dtos/account-dto/AccountBalanceDTO.js');
 
-const { asyncHandler } = require('../middlewares/error/errorHandler.js');
-
-const accountServices = new AccountServices();
-
 class AccountController extends Controller {
   constructor() {
-    super(accountServices, { CreateDTO: AccountInputDTO, UpdateDTO: AccountUpdateDTO });
+    super(new AccountServices(), {
+      CreateDTO: AccountInputDTO,  
+      UpdateDTO: AccountUpdateDTO, 
+    });
   }
 
   findByAccountNumber = asyncHandler(async (req, res) => {
-    const account = await accountServices.findByNumber(req.params.accountNumber);
-    return res.status(200).json(account); 
+    const ctx = this.buildCtx(req);
+    const { accountNumber } = req.params;
+    const account = await this.entityService.findByNumber(accountNumber, ctx);
+    return res.status(200).json(account);
   });
 
   changeBalance = asyncHandler(async (req, res) => {
+    const ctx = this.buildCtx(req);
+    const { accountId } = req.params;
     const balanceDTO = new AccountBalanceDTO(req.body.balance);
-    const result = await accountServices.changeBalance(req.params.accountId, balanceDTO.balance);
-    return res.status(200).json(result); 
+    const result = await this.entityService.changeBalance(Number(accountId), balanceDTO.balance, ctx);
+    return res.status(200).json(result);
   });
 
-  listMine = asyncHandler(async (req, res) => {
-    const myId = Number(req.params.myId);
-    const list = await accountServices.findAllByOwner(myId);
-    return res.status(200).json(list); 
+  findAll = asyncHandler(async (req, res) => {
+    const ctx = this.buildCtx(req);
+    const customerId = Number(req.params.myId);
+    const list = await this.entityService.findAll(customerId, ctx);
+    return res.status(200).json(list);
   });
 
-  getMineById = asyncHandler(async (req, res) => {
-    const myId = Number(req.params.myId);
+  findById = asyncHandler(async (req, res) => {
+    const ctx = this.buildCtx(req);
+    const customerId = Number(req.params.myId);
     const accountId = Number(req.params.accountId);
-    const acc = await accountServices.findOwnedById(myId, accountId);
-    return res.status(200).json(acc); 
+    const record = await this.entityService.findById(accountId, customerId, ctx);
+    return res.status(200).json(record);
   });
 
-  createMine = asyncHandler(async (req, res) => {
-    const myId = Number(req.params.myId);
-    const dto = new AccountInputDTO(req.body);
-    const created = await accountServices.createOwned(myId, dto);
+  create = asyncHandler(async (req, res) => {
+    const ctx = this.buildCtx(req);
+    const customerId = Number(req.params.myId);
+    const dto = new this.CreateDTO({ customerId, type: req.body.type });
+    const created = await this.entityService.create(dto, ctx); 
     return res.status(201).json(created);
   });
 
-  updateMine = asyncHandler(async (req, res) => {
-    const myId = Number(req.params.myId);
+  update = asyncHandler(async (req, res) => {
+    const ctx = this.buildCtx(req);
+    const customerId = Number(req.params.myId);
     const accountId = Number(req.params.accountId);
-    const dto = new AccountUpdateDTO(req.body);
-    const updated = await accountServices.updateOwned(myId, accountId, dto);
-    return res.status(200).json(updated); 
+    const dto = new this.UpdateDTO({ type: req.body.type });
+    const updated = await this.entityService.update(dto, accountId, customerId, ctx);
+    return res.status(200).json(updated);
   });
 
-  removeMine = asyncHandler(async (req, res) => {
-    const myId = Number(req.params.myId);
+  delete = asyncHandler(async (req, res) => {
+    const ctx = this.buildCtx(req);
+    const customerId = Number(req.params.myId);
     const accountId = Number(req.params.accountId);
-    await accountServices.deleteOwned(myId, accountId);
+    await this.entityService.delete(accountId, customerId, ctx);
     return res.status(200).json({ message: `Account ${accountId} deleted` });
   });
 }
