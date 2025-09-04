73847b6933d9a9be444c6a9299357d98656096cc
Alex Rodrigues <alex-xp123@hotmail.com>
2025-09-04 10:25:21 -0400

    refactor(controller): add ctx propagation to all controller methods

--- customers-accounts-ms/src/controllers/Controller.js
+++ customers-accounts-ms/src/controllers/Controller.js
@@ -8,21 +8,33 @@ class Controller {
     this.UpdateDTO = UpdateDTO;
   }
 
-  getAll = asyncHandler(async (req, res) => {
-    const list = await this.entityService.findAll();
+  buildCtx(req) {
+    return {
+      transaction: null,
+      user: req.user || null,
+      correlationId: req.headers['x-correlation-id'],
+      raw: false,
+    };
+  }
+
+  findAll = asyncHandler(async (req, res) => {
+    const ctx = this.buildCtx(req);
+    const list = await this.entityService.findAll(ctx);
     return res.status(200).json(list);
   });
 
-  getById = asyncHandler(async (req, res) => {
+  findById = asyncHandler(async (req, res) => {
+    const ctx = this.buildCtx(req);
     const { id } = req.params;
-    const record = await this.entityService.findById(Number(id));
+    const record = await this.entityService.findById(Number(id), ctx);
     return res.status(200).json(record);
   });
 
   create = asyncHandler(async (req, res) => {
+    const ctx = this.buildCtx(req);
     try {
       const body = this.CreateDTO ? new this.CreateDTO(req.body) : req.body;
-      const created = await this.entityService.create(body);
+      const created = await this.entityService.create(body, ctx);
       return res.status(201).json(created);
     } catch (err) {
       if (err instanceof yup.ValidationError) {
@@ -33,17 +45,23 @@ class Controller {
   });
 
   update = asyncHandler(async (req, res) => {
+    const ctx = this.buildCtx(req);
     const { id } = req.params;
     const body = this.UpdateDTO ? new this.UpdateDTO(req.body) : req.body;
-    const updated = await this.entityService.update(body, Number(id));
+    const updated = await this.entityService.update(body, Number(id), ctx);
     return res.status(200).json(updated);
   });
 
-  remove = asyncHandler(async (req, res) => {
+  delete = asyncHandler(async (req, res) => {
+    const ctx = this.buildCtx(req);
     const { id } = req.params;
-    await this.entityService.delete(Number(id));
+    await this.entityService.delete(Number(id), ctx);
     return res.status(200).json({ message: `ID ${id} deleted` });
   });
+
+  getAll = this.findAll;
+  getById = this.findById;
+  remove = this.delete;
 }
 
 module.exports = Controller;
